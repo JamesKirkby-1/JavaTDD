9de7b3fc708db7bac46eb20327480941b8d7251d
kirkb <kirkbyjkk@gmail.com>
2022-11-26 16:51:53 +0000

    Created a test method called testRGBLessThanZero to check if any RGB value is less than 0.0

--- src/test/java/TestColour.java
+++ src/test/java/TestColour.java
@@ -1,2 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertThrows;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+/**
+ * The TestColour class will be used for test-driven development of the Colour class.
+ */
+
 public class TestColour {
+
+    /**
+     * Test method to check that an IllegalArgumentException is thrown when any RGB value is less than 0.
+     */
+    @Test
+    void RGBLessThanZero() {
+
+        Exception RGBLessThanZero = assertThrows(IllegalArgumentException.class, () -> {
+            Colour colour = new Colour(-1, 0, 0);
+        });
+        Assertions.assertEquals("RGB values must be between 0.0 and 1.0", RGBLessThanZero.getMessage());
+    }
 }
