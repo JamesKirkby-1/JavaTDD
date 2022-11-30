248d89445ad52b438e26b2c5349a22c266d44db3
kirkb <kirkbyjkk@gmail.com>
2022-11-30 12:10:09 +0000

    Added getters and setters for red, green, and blue values

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -10,9 +10,10 @@ public class Colour {
 
     /**
      * Constructor for the Colour class.
-     * @param red The red value of the colour.
+     *
+     * @param red   The red value of the colour.
      * @param green The green value of the colour.
-     * @param blue The blue value of the colour.
+     * @param blue  The blue value of the colour.
      */
 
     public Colour(float red, float green, float blue) {
@@ -24,15 +25,16 @@ public class Colour {
         if (red < 0 || green < 0 || blue < 0) {
             throw new IllegalArgumentException("RGB values must equal to or greater than 0.0");
         }
-        if (red > 1 || green > 1|| blue > 1) {
+        if (red > 1 || green > 1 || blue > 1) {
             throw new IllegalArgumentException("RGB values must be equal to or less than 1.0");
         }
     }
 
     /**
      * Constructor for the hex colour class.
+     *
      * @param hex The hex value of the colour, where the first two characters are the red value, the
-     * second two characters are the green value and the last two characters are the blue value.
+     *            second two characters are the green value and the last two characters are the blue value.
      */
     public Colour(String hex) {
 
@@ -47,5 +49,62 @@ public class Colour {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Invalid hex value");
         }
+    }
+
+    /**
+     * Method to get the red value of the colour.
+     * @return The red value of the colour.
+     */
+    public float getRed() {
+        return red;
+    }
+
+    /**
+     * Method to get the green value of the colour.
+     * @return The green value of the colour.
+     */
+    public float getGreen() {
+        return green;
+    }
+
+    /**
+     * Method to get the blue value of the colour.
+     * @return The blue value of the colour.
+     */
+    public float getBlue() {
+        return blue;
+    }
+
+    /**
+     * Method to set the red value of the colour.
+     * @param red The red value of the colour.
+     */
+    public void setRed(float red) {
+        if (red < 0 || red > 1) {
+            throw new IllegalArgumentException("RGB values must be equal to or less than 1.0");
+        }
+        this.red = red;
+    }
+
+    /**
+     * Method to set the red value of the colour.
+     * @param green The red value of the colour.
+     */
+    public void setGreen(float green) {
+        if (green < 0 || green > 1) {
+            throw new IllegalArgumentException("RGB values must be equal to or less than 1.0");
+        }
+        this.green = green;
+    }
+
+    /**
+     * Method to set the red value of the colour.
+     * @param blue The red value of the colour.
+     */
+    public void setBlue(float blue) {
+        if (blue < 0 || blue > 1) {
+            throw new IllegalArgumentException("RGB values must be equal to or less than 1.0");
+        }
+        this.blue = blue;
     }
 }
