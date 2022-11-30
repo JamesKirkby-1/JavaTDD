b45452a1dc492691c1dc25b3f074cbc05f382973
kirkb <kirkbyjkk@gmail.com>
2022-11-30 11:52:49 +0000

    Created a constructor for a hex representation

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -10,6 +10,9 @@ public class Colour {
 
     /**
      * Constructor for the Colour class.
+     * @param red The red value of the colour.
+     * @param green The green value of the colour.
+     * @param blue The blue value of the colour.
      */
 
     public Colour(float red, float green, float blue) {
@@ -18,11 +21,31 @@ public class Colour {
         this.green = green;
         this.blue = blue;
 
-        if (red < 0.F || green < 0.F || blue < 0.F) {
+        if (red < 0 || green < 0 || blue < 0) {
             throw new IllegalArgumentException("RGB values must equal to or greater than 0.0");
         }
-        if (red > 1.0F || green > 1.0F || blue > 1.0F) {
+        if (red > 1 || green > 1|| blue > 1) {
             throw new IllegalArgumentException("RGB values must be equal to or less than 1.0");
+        }
+    }
+
+    /**
+     * Constructor for the hex colour class.
+     * @param hex The hex value of the colour, where the first two characters are the red value, the
+     * second two characters are the green value and the last two characters are the blue value.
+     */
+    public Colour(String hex) {
+
+        if (hex.length() != 6) {
+            throw new IllegalArgumentException("Invalid hex value");
+        }
+
+        try {
+            this.red = Integer.parseInt(hex.substring(0, 2), 16) / 255f;
+            this.green = Integer.parseInt(hex.substring(2, 4), 16) / 255f;
+            this.blue = Integer.parseInt(hex.substring(4, 6), 16) / 255f;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid hex value");
         }
     }
 }
